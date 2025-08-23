b4912f35fa6c5c2532e2e26d94b1182e745cce8e
nomanseo <dev@serpnexus.com>
2025-08-23 17:37:01 +0000

    feat: Update layouts and pages for improved structure and functionality
    - Set default language to English in BlogPostUnicode layout.
    - Added Google Ads script to BlogPostUnicode, Layout, and StaticPageLayout.
    - Refactored blog post page to enhance readability and maintainability.
    - Updated contact email address in contact page.
    - Created a new disclaimer page with relevant content.
    - Adjusted index and tool pages to use StaticPageLayout for consistency.
    - Implemented server-side rendering for various components on the index and tool pages.
    - Updated TypeScript configuration to reference local astro tsconfig.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-
 import vercel from '@astrojs/vercel';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -35,18 +35,20 @@ export default defineConfig({
   ],
 
   output: 'server',
+  adapter: vercel(),
   site: 'https://www.invisibletext.me/',
 
   i18n: {
-    defaultLocale: 'en',
     locales: [
       'en', 'es', 'fr', 'de', 'pt', 'ru', 'ko', 'tr', 'nl', 'it', 'id', 'my', 'fi', 'ja', 'hu', 'vi', 'th'
     ],
+    defaultLocale: 'en',
     routing: {
       prefixDefaultLocale: false
     }
   },
 
   trailingSlash: 'never',
-  adapter: vercel(),
+  
+  compressHtml: false,
 });
