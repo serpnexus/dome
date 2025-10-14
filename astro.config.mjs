154831fe62bde6ce7f7cae8d292fdb898763b906
nomanseo <dev@serpnexus.com>
2025-10-14 23:47:21 +0000

    Refactor layout and improve accessibility
    - Updated UnicodeTable.astro to enhance layout responsiveness.
    - Modified Layout.astro to correct hreflang links and improve structured data for SEO.
    - Adjusted index.astro to include additional languages for static paths.
    - Enhanced blog index page title for better clarity and SEO.
    - Removed unnecessary headings from index.astro for a cleaner layout.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,49 +5,21 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import vercel from '@astrojs/vercel';
 
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), sitemap({
-    i18n: {
-      defaultLocale: 'en',
-      locales: {
-        'en': 'English',
-        'es': 'Spanish',
-        'fr': 'French',
-        'de': 'German',
-        'pt': 'Portuguese',
-        'ru': 'Russian',
-        'ko': 'Korean',
-        'tr': 'Turkish',
-        'nl': 'Dutch',
-        'it': 'Italian',
-        'id': 'Indonesian',
-        'my': 'Malay',
-        'fi': 'Finnish',
-        'ja': 'Japanese',
-        'hu': 'Hungarian',
-        'vi': 'Vietnamese',
-        'th': 'Thai',
-      }
-    }
-  }), tailwind(), mdx()],
-
+  integrations: [react(), sitemap(), tailwind(), mdx()],
   output: 'server',
   adapter: vercel(),
   site: 'https://www.invisibletext.me',
-
   i18n: {
+    defaultLocale: 'en',
     locales: [
       'en', 'es', 'fr', 'de', 'pt', 'ru', 'ko', 'tr', 'nl', 'it', 'id', 'my', 'fi', 'ja', 'hu', 'vi', 'th'
     ],
-    defaultLocale: 'en',
     routing: {
       prefixDefaultLocale: false
     }
   },
-
   trailingSlash: 'never',
-  
   compressHtml: true,
 });
